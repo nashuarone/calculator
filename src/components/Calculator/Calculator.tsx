cc6e427ca4da87f72e6d681d2e6249bf99b250c2
taraswww777 <taraswww777@mail.ru>
2023-05-12 19:48:57 +0300

    feat: add input

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import {Button, Col, Layout, Row} from 'antd';
+import {Button, Col, Input, Layout, Row} from 'antd';
 
 export const Calculator = () => {
   const numberActions: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -11,7 +11,10 @@ export const Calculator = () => {
       margin: '0 auto',
     }}>
       <Layout.Content>
-        <Row>
+        <Row gutter={[0, 8]}>
+          <Col span={24}>
+            <Input size={'large'} value={3} disabled />
+          </Col>
           <Col span={20}>
             <Row gutter={[8, 8]}>
               {numberActions.map(n => (
